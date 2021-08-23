51d9bc94e38d87bed182eb359071a79020196814
Isher Sagay <ishersagay@gmail.com>
2021-08-23 16:35:01 +0100

    Added the functionality, however I need to ensure that it looks like a calculator

--- main.js
+++ main.js
@@ -1,21 +1,21 @@
 const calculator = document.querySelector('.calculator')
 const keys = document.querySelector('.calculator__keys')
-const display = document.querySelector('.calculator__display')
-let bitch = " ";
+const display = document.querySelector(".calculator__display")
+let placeHolder = " ";
 let numArray = [];
 let tally = 0;
-let sum = 0;
+let finalSum = 0;
 let hasEqualsBeenClicked = false;
+let method = " ";
 
 const plus = () => {
-
     if (hasEqualsBeenClicked === true){
-        sum = numArray.reduce(function (a,b){
+        finalSum = numArray.reduce(function (a,b){
             return a+b;
         }, 0)
-        console.log(sum)
+        display.innerHTML = finalSum
     } else {
-
+        method = "plus"
         let firstNumber = parseInt(store());
         console.log(firstNumber)
         numArray[tally] = firstNumber
@@ -24,22 +24,82 @@ const plus = () => {
     }
 }
 
-const minus = () => {return console.log("minus button works");}
-const multiplication = () => {console.log("multiplication button works");}
-const division = () => {console.log("divison works ");}
+const minus = () => {
+    if (hasEqualsBeenClicked === true) {
+        finalSum = numArray[0] - numArray[1];
+        display.innerHTML = finalSum
+    } else {
+        method = "minus"
+        let firstNumber = parseInt(store());
+        console.log(firstNumber)
+        numArray[tally] = firstNumber
+        reset();
+        tally += 1;
+    }
+}
+
+const multiplication = () => {
+    if (hasEqualsBeenClicked === true) {
+        finalSum = numArray[0] * numArray[1]
+        console.log(finalSum)
+        display.innerHTML = finalSum;
+    } else {
+        method = "multiplication"
+        let firstNumber = parseInt(store());
+        console.log(firstNumber);
+        numArray[tally] = firstNumber;
+        reset();
+        tally += 1;
+    }
+}
+
+const division = () => {
+    if (hasEqualsBeenClicked === true) {
+        finalSum = numArray[0] / numArray[1];
+        display.innerHTML = finalSum;
+    } else {
+        method = "division"
+        let firstNumber = parseInt(store());
+        console.log(firstNumber);
+        numArray[tally] = firstNumber;
+        reset();
+        tally += 1;
+    }
+}
 const reset = () => {
    display.innerHTML = 0;
-   bitch = " ";
+   placeHolder = " ";
+}
+const fullReset = () => {
+    display.innerHTML = 0;
+    placeHolder = " ";
+    tally = 0;
+    finalSum = 0;
+    hasEqualsBeenClicked = false;
 }
 const equals = () => {
     hasEqualsBeenClicked = true
-    plus()
+    switch (method) {
+        case "plus":
+            plus();
+            break;
+        case "minus":
+            minus();
+            break;
+        case "division":
+            division();
+            break;
+        case "multiplication":
+            multiplication();
+            break;
+    }
 }
 const store = (value) => {
  let tempNumber = value;
-    bitch += tempNumber
-   return bitch;
+    placeHolder += tempNumber
+   return placeHolder;
 }
+
 keys.addEventListener("click",e => {
     display.innerHTML += e.target.value;
   store(e.target.value);
