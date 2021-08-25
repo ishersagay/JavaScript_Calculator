04549c20cf8536a981be9c7a112e96d30d7112d2
Isher Sagay <ishersagay@gmail.com>
2021-08-25 11:05:51 +0100

    I've made the design look like an actual calculator, secondly I've changed my js again to try and have a better solution than the first attempt, I will still need to add in the functionality, but given a bit more time I will be capable

--- main.js
+++ main.js
@@ -1,6 +1,5 @@
-const calculator = document.querySelector('.calculator')
-const keys = document.querySelector('.calculator__keys')
-const display = document.querySelector(".calculator__display")
+const keys = document.querySelector('.calculator-keys');
+const display = document.querySelector('.calculator-screen')
 let placeHolder = " ";
 let numArray = [];
 let tally = 0;
@@ -9,98 +8,60 @@ let hasEqualsBeenClicked = false;
 let method = " ";
 
 const plus = () => {
-    if (hasEqualsBeenClicked === true){
-        finalSum = numArray.reduce(function (a,b){
-            return a+b;
-        }, 0)
-        display.innerHTML = finalSum
-    } else {
-        method = "plus"
-        let firstNumber = parseInt(store());
-        console.log(firstNumber)
-        numArray[tally] = firstNumber
-        reset()
-        tally += 1;
-    }
+    arrayStore();
 }
 
-const minus = () => {
-    if (hasEqualsBeenClicked === true) {
-        finalSum = numArray[0] - numArray[1];
-        display.innerHTML = finalSum
-    } else {
-        method = "minus"
-        let firstNumber = parseInt(store());
-        console.log(firstNumber)
-        numArray[tally] = firstNumber
-        reset();
-        tally += 1;
-    }
-}
+const minus = () => {}
 
-const multiplication = () => {
-    if (hasEqualsBeenClicked === true) {
-        finalSum = numArray[0] * numArray[1]
-        console.log(finalSum)
-        display.innerHTML = finalSum;
-    } else {
-        method = "multiplication"
-        let firstNumber = parseInt(store());
-        console.log(firstNumber);
-        numArray[tally] = firstNumber;
-        reset();
-        tally += 1;
-    }
-}
+const division = () => {}
 
-const division = () => {
-    if (hasEqualsBeenClicked === true) {
-        finalSum = numArray[0] / numArray[1];
-        display.innerHTML = finalSum;
-    } else {
-        method = "division"
-        let firstNumber = parseInt(store());
-        console.log(firstNumber);
-        numArray[tally] = firstNumber;
-        reset();
-        tally += 1;
-    }
-}
-const reset = () => {
-   display.innerHTML = 0;
-   placeHolder = " ";
-}
-const fullReset = () => {
+const multiplication = () => {}
+
+const allClear = () => {
     display.innerHTML = 0;
     placeHolder = " ";
     tally = 0;
     finalSum = 0;
     hasEqualsBeenClicked = false;
 }
-const equals = () => {
-    hasEqualsBeenClicked = true
-    switch (method) {
-        case "plus":
-            plus();
-            break;
-        case "minus":
-            minus();
-            break;
-        case "division":
-            division();
-            break;
-        case "multiplication":
-            multiplication();
-            break;
-    }
+
+const reset = () => {
+    display.innerHTML = 0;
+    placeHolder = " ";
 }
+
+const arrayStore = () => {
+    let firstNumber = parseInt(store());
+    console.log(firstNumber);
+    numArray[tally] = firstNumber;
+    reset();
+    tally += 1;
+}
+
 const store = (value) => {
- let tempNumber = value;
-    placeHolder += tempNumber
-   return placeHolder;
+    let tempNumber = value;
+    placeHolder += tempNumber;
+    return placeHolder;
 }
 
 keys.addEventListener("click",e => {
-    display.innerHTML += e.target.value;
-  store(e.target.value);
+    if (e.target.value === "+") {
+        plus();
+    } else if (e.target.value === "-") {
+        minus();
+        display.innerHTML += e.target.value;
+    } else if (e.target.value === "/") {
+        division();
+        display.innerHTML += e.target.value;
+    } else if (e.target.value === "*") {
+        multiplication();
+        display.innerHTML += e.target.value;
+    } else if (e.target.value === "all-clear") {
+        allClear();
+    } else if (e.target.value === "") {
+
+    } else {
+        display.innerHTML += e.target.value;
+        console.log(store(e.target.value));
+    }
 })
